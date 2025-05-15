d23647696bf6da5429a8f238f423a2749b9442cf
praveenbchoudari98 <your-email@example.com>
2025-05-15 22:23:41 +0530

    fix: add test page and simplify asset configuration

--- vite.config.ts
+++ vite.config.ts
@@ -15,22 +15,9 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/media/[name].[hash][extname]`
-          }
-          if (/\.(png|jpe?g|gif|svg|ico|webp)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/images/[name].[hash][extname]`
-          }
-          if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/i.test(assetInfo.name)) {
-            return `assets/fonts/[name].[hash][extname]`
-          }
-          return `assets/[name].[hash][extname]`
-        },
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        entryFileNames: 'assets/js/[name].[hash].js'
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
       }
     }
   }
