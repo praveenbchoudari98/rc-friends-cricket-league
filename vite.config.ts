46a889906e24c84342b056e86d7d1e9d4f1c0cd5
praveenbchoudari98 <your-email@example.com>
2025-05-15 22:27:45 +0530

    fix: update deployment configuration and add proper 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rc-friends-cricket-league/',
+  base: '',
   server: {
     port: 5175,
   },
